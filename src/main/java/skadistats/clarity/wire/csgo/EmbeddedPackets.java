d0ef8f850ae10363550a1b64594f2727a98a9021
Martin Schrodt <clarity@martin.schrodt.org>
2018-04-02 21:19:26 +0200

    special version of server info for CSGO

---
+++ src/main/java/skadistats/clarity/wire/csgo/EmbeddedPackets.java
@@ -0,0 +1,30 @@
+package skadistats.clarity.wire.csgo;
+
+import com.google.protobuf.GeneratedMessage;
+import skadistats.clarity.wire.common.proto.NetMessages;
+import skadistats.clarity.wire.common.proto.NetworkBaseTypes;
+import skadistats.clarity.wire.csgo.proto.CsGoNetMessages;
+import skadistats.clarity.wire.s1.proto.S1NetMessages;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmbeddedPackets {
+
+    private static final Map<Integer, Class<? extends GeneratedMessage>> OVERRIDES;
+    static {
+        OVERRIDES = new HashMap<>();
+
+        /*  8 */ OVERRIDES.put(S1NetMessages.SVC_Messages.svc_ServerInfo_VALUE, CsGoNetMessages.CSVCMsg_ServerInfo.class);
+
+    }
+
+    public static Class<? extends GeneratedMessage> classForKind(int kind) {
+        Class<? extends GeneratedMessage> cls = OVERRIDES.get(kind);
+        if (cls == null) {
+            cls = skadistats.clarity.wire.s1.EmbeddedPackets.classForKind(kind);
+        }
+        return cls;
+    }
+
+}
