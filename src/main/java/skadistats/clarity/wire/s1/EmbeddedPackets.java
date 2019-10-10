21bf175d6247b6555d5bee1243b3ff1a9766660b
Martin Schrodt <clarity@martin.schrodt.org>
2019-10-10 10:15:59 +0200

    Synchronize all work

--- src/main/java/skadistats/clarity/wire/s1/EmbeddedPackets.java
+++ src/main/java/skadistats/clarity/wire/s1/EmbeddedPackets.java
@@ -13,6 +13,7 @@ public class EmbeddedPackets {
     private static final Map<Integer, Class<? extends GeneratedMessage>> EMBED;
     static {
         EMBED = new HashMap<>();
+        /*   0 */ EMBED.put(NetworkBaseTypes.NET_Messages.net_NOP_VALUE, NetworkBaseTypes.CNETMsg_NOP.class);
 
         /*   4 */ EMBED.put(NetworkBaseTypes.NET_Messages.net_Tick_VALUE, NetworkBaseTypes.CNETMsg_Tick.class);
         /*   5 */ EMBED.put(NetworkBaseTypes.NET_Messages.net_StringCmd_VALUE, NetworkBaseTypes.CNETMsg_StringCmd.class);
