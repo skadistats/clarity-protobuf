b7daddd6c999dfe38fc7f5486d48dac66686e236
Martin Schrodt <martin@schrodt.org>
2020-10-30 14:17:51 +0100

    fix protobuf include path

--- src/main/proto/csgo/make.sh
+++ src/main/proto/csgo/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/opt/protobuf2/bin/protoc --proto_path=/usr/include/protobuf2 --proto_path=../common --proto_path=. --java_out=../../java cstrike*.proto
-/opt/protobuf2/bin/protoc --proto_path=/usr/include/protobuf2 --proto_path=. --java_out=../../java csgo*.proto
+/opt/protobuf2/bin/protoc --proto_path=/opt/protobuf2/include --proto_path=../common --proto_path=. --java_out=../../java cstrike*.proto
+/opt/protobuf2/bin/protoc --proto_path=/opt/protobuf2/include --proto_path=. --java_out=../../java csgo*.proto
--- src/main/proto/s2/make.sh
+++ src/main/proto/s2/make.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/protobuf2/bin/protoc --proto_path=/usr/include/protobuf2 --proto_path=../common --proto_path=. --java_out=../../java *.proto
+/opt/protobuf2/bin/protoc --proto_path=/opt/protobuf2/include --proto_path=../common --proto_path=. --java_out=../../java *.proto
