7b0d46917e50a959e2898e79de4b18ecae7d803b
Martin Schrodt <martin@schrodt.org>
2023-06-01 18:02:35 +0300

    switch build system to gradle (still need to document)

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+}
+
+group = "com.skadistats"
+version = "5.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+// TODO: this still does not suppress the messages, but seems to ignore errors on publishing
+tasks.withType<Javadoc> {
+    options {
+        this as StandardJavadocDocletOptions
+        addStringOption("Xdoclint:all,-missing")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("it.unimi.dsi:fastutil:8.5.11")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set(rootProject.name)
+                description.set("Clarity is an open source replay parser for Dota 2 and CSGO 1 and 2 written in Java. This JAR contains the protobuf classes for clarity.")
+                url.set("https://github.com/skadistats/clarity-protobuf")
+                licenses {
+                    license {
+                        name.set("BSD style license")
+                        url.set("https://github.com/skadistats/clarity-protobuf/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("spheenik")
+                        name.set("Martin Schrodt")
+                        email.set("github@martin.schrodt.org")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:skadistats/clarity-protobuf.git")
+                    developerConnection.set("scm:git:git@github.com:skadistats/clarity-protobuf.git")
+                    url.set("https://github.com/skadistats/clarity-protobuf")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    this.repositories {
+        sonatype()
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
+
